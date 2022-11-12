6071af35f5ccd8c9af30512a00cbdd406784254d
antonio.trupac <sniper.trupac@gmail.com>
2022-11-12 10:26:55 +0100

    add button sizes

--- ekocell/src/components/Button/SubmitButton.tsx
+++ ekocell/src/components/Button/SubmitButton.tsx
@@ -2,15 +2,25 @@ import classnames from "classnames";
 
 import classes from "./Button.module.scss";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: "s" | "m";
+}
 
-const SubmitButton = ({ children, className, type, ...props }: ButtonProps) => {
+const SubmitButton = ({
+  children,
+  className,
+  size = "m",
+  type,
+  ...props
+}: ButtonProps) => {
   return (
     <button
       className={classnames(
         className,
-        "px-4 py-1 bg-button text-white rounded-[4px] flex items-end justify-end",
-        classes["submit-button"]
+        "bg-button text-white rounded-[4px] flex items-end justify-end",
+        classes["submit-button"],
+        [size === "s" && "px-4 py-1"],
+        [size === "m" && "px-6 py-2"]
       )}
       type={type}
       {...props}
